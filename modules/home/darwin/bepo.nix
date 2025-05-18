45c1e4e50d7f037d64b01f822d8c8289abacfe6b
Hubert Behaghel <behaghel@gmail.com>
2025-05-18 19:42:04 +0200

    cleanup: remove old dotfiles references

--- modules/home/darwin/bepo.nix
+++ modules/home/darwin/bepo.nix
@@ -1,9 +1,3 @@
-{lib, ...}:
-{
-  home.activation = {
-    # macos doesn't support symlink for keyboard layouts
-    copyBepoLayout = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-                       $DRY_RUN_CMD cp ".dotfiles/macos/Library/Keyboard Layouts/bepo.keylayout" $HOME/Library/Keyboard\ Layouts
-      '';
-  };
-}
+{ lib, ... }:
+
+{}
