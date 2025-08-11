12b60b9e39182c9cbaf5072b6dac7ca02166b08e
Hubert Behaghel <behaghel@gmail.com>
2025-08-11 11:12:05 +0200

    [dev] Add Veriff laptop home conf + dev module with devenv

---
+++ configurations/home/f2200346.nix
@@ -0,0 +1,21 @@
+{ flake, ... }:
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in
+{
+  imports = [
+    self.homeModules.default
+    self.homeModules.linux-only
+    self.homeModules.dev
+  ];
+  # Defined by /modules/home/me.nix
+  # And used all around in /modules/home/*
+  me = {
+    username = "hubertbehaghel";
+    fullname = "Hubert Behaghel";
+    email = "hubert.behaghel@veriff.net";
+  };
+
+  home.stateVersion = "24.11";
+}
