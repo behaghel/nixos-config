90c1e689b923d5f787b20009aae2aa4761ed9079
Hubert Behaghel <behaghel@gmail.com>
2025-08-14 08:27:55 +0000

    [hubertbehaghel] stop trying GL-based apps via nix

--- configurations/home/hubertbehaghel.nix
+++ configurations/home/hubertbehaghel.nix
@@ -8,7 +8,6 @@ in
     self.homeModules.default
     self.homeModules.linux-only
     self.homeModules.dev
-    self.homeModules.alacritty
   ];
   # Defined by /modules/home/me.nix
   # And used all around in /modules/home/*
@@ -19,15 +18,4 @@ in
   };
 
   home.stateVersion = "24.11";
-
-  # Linux-specific Alacritty configuration to fix OpenGL issues
-  programs.alacritty.settings = {
-    general.live_config_reload = true;
-    env.LIBGL_ALWAYS_SOFTWARE = "1";
-  };
-
-  # Ensure Mesa software rendering is available on Linux
-  home.packages = with pkgs; [
-    mesa
-  ];
 }
