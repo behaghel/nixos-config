415d86b7e3f93a68a7c226f4e88e71efd220d068
Hubert Behaghel <behaghel@gmail.com>
2025-10-31 20:20:35 +0100

    [pass] ensure password store

--- modules/home/password-store/default.nix
+++ modules/home/password-store/default.nix
@@ -1,11 +1,24 @@
 
-{ ... }:
+{ lib, pkgs, ... }:
 
+let
+  passwordStoreDir = "$HOME/.password-store";
+  repoUrl = "git@gitlab.com:behaghel/pass.git";
+in
 {
   programs.password-store = {
     enable = true;
     settings = {
-      PASSWORD_STORE_DIR = "$HOME/.password-store";
+      PASSWORD_STORE_DIR = passwordStoreDir;
     };
   };
+
+  home.activation.ensurePasswordStore = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -d "${passwordStoreDir}/.git" ]; then
+      rm -rf "${passwordStoreDir}"
+      ${pkgs.git}/bin/git clone "${repoUrl}" "${passwordStoreDir}"
+    else
+      ${pkgs.git}/bin/git -C "${passwordStoreDir}" fetch --all --prune
+    fi
+  '';
 }
