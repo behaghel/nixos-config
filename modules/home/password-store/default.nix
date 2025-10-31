0d622da7684ace461db9673f95f368fd93ccac40
Hubert Behaghel <behaghel@gmail.com>
2025-10-31 20:24:36 +0100

    [pass] always use full path in activation scripts

--- modules/home/password-store/default.nix
+++ modules/home/password-store/default.nix
@@ -4,6 +4,8 @@
 let
   passwordStoreDir = "$HOME/.password-store";
   repoUrl = "git@gitlab.com:behaghel/pass.git";
+  gitBin = "${pkgs.git}/bin/git";
+  sshCommand = "${pkgs.openssh}/bin/ssh -F /dev/null";
 in
 {
   programs.password-store = {
@@ -16,9 +18,9 @@ in
   home.activation.ensurePasswordStore = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -d "${passwordStoreDir}/.git" ]; then
       rm -rf "${passwordStoreDir}"
-      ${pkgs.git}/bin/git clone "${repoUrl}" "${passwordStoreDir}"
+      GIT_SSH_COMMAND="${sshCommand}" ${gitBin} clone "${repoUrl}" "${passwordStoreDir}"
     else
-      ${pkgs.git}/bin/git -C "${passwordStoreDir}" fetch --all --prune
+      GIT_SSH_COMMAND="${sshCommand}" ${gitBin} -C "${passwordStoreDir}" fetch --all --prune
     fi
   '';
 }
