5ec3a3921a1eb19e2615591a93f1917afc71365c
Hubert Behaghel <behaghel@gmail.com>
2026-04-27 09:41:25 +0200

    home: expand texlive packages for org export
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- modules/home/texlive/default.nix
+++ modules/home/texlive/default.nix
@@ -4,6 +4,43 @@
 {
   programs.texlive = {
     enable = true;
-    extraPackages = tpkgs: { inherit (tpkgs) scheme-basic wrapfig amsmath ulem hyperref capt-of xcolor dvisvgm dvipng metafont; };
+    extraPackages = tpkgs: {
+      inherit (tpkgs)
+        scheme-basic
+        wrapfig
+        amsmath
+        ulem
+        hyperref
+        capt-of
+        xcolor
+        dvisvgm
+        dvipng
+        metafont
+        # Minimum blockers / current prototype needs
+        pgf
+        tcolorbox
+        listings
+        titlesec
+        enumitem
+        # Strongly recommended extras
+        caption
+        float
+        setspace
+        ragged2e
+        xurl
+        fancyvrb
+        fvextra
+        upquote
+        eso-pic
+        background
+        titling
+        lastpage
+        # Nice-to-have richer variants
+        fontawesome5
+        mdframed
+        framed
+        pgfplots
+        ;
+    };
   };
 }
